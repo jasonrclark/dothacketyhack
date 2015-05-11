2f0133d9e005236e8d1530ef2c4d4ddc7cef062a
Jason R. Clark <jclark@newrelic.com>
2015-05-10 20:36:54 -0700

    Demo in Coraline's class.

---
+++ Classroom.rb
@@ -0,0 +1,12 @@
+Shoes.app do
+  rect 100, 100, 100, 200, fill: blue
+  oval 125, 50, 50, 50, fill: green
+  line 200, 200, 250, 100, strokewidth: 8
+  line 100, 200, 50, 100, strokewidth: 8
+  
+  rect 80, 400, 50, 25, fill: red
+  rect 170, 400, 50, 25, fill: red  
+  
+  line 100, 300, 100, 400, strokewidth: 12
+  line 200, 300, 200, 400, strokewidth: 12
+end
