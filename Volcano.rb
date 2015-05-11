7a23ecefa710bdc50d3a911686cf568a75f836ae
Jason R. Clark <jclark@newrelic.com>
2015-05-10 20:37:30 -0700

    Volcano, WIP

---
+++ Volcano.rb
@@ -0,0 +1,10 @@
+Shoes.app do
+  shape fill: orangered, strokewidth: 5 do
+    move_to 100, 500
+    line_to 200, 200
+    line_to 350, 200
+    line_to 450, 500
+    line_to 100, 500
+  end
+  # red for lava, orangered for volcano
+end
