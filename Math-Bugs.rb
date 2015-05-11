8cfdbb10e12c729ad494cd903e0169dd9ba872c1
Jason R. Clark <jclark@newrelic.com>
2015-05-10 20:36:40 -0700

    Math-Bugs checkpoint

--- Math-Bugs.rb
+++ Math-Bugs.rb
@@ -1,17 +1,26 @@
 Shoes.app do
+  # Worms!
   @worms = []
-  @worms << [oval(100, 100, 300, 40, fill: lightcoral), 2,2]
-  @worms << [oval(90,  200, 300, 40, fill: magenta), 3, 3]
-  @worms << [oval(100, 300, 300, 40, fill: navy), 4, 4]
+  @worms << [oval(200, 100, 300, 40, fill: lightcoral), 2,2, para("")]
+  @worms << [oval(200, 200, 300, 40, fill: magenta), 3, 3, para("")]
+  @worms << [oval(200, 300, 300, 40, fill: navy), 4, 4, para("")]
   
-  line 110, 112, 110, 120
-  line 135, 107, 135, 120
-  line 160, 103, 160, 120
-
   @worms.each_with_index do |worm, index|
-    worm << para("#{worm[-1]}+#{worm[-2]}=", left: 200, top: (100 * (index + 1) + 10), stroke: white)
+    p = worm.last
+    p.text = "#{worm[-2]}+#{worm[-3]}="
+    p.left = 300
+    p.top =  (100 * (index + 1) + 10)
+    p.stroke = white
+    p.hide
+  end
+  
+  # Finish Line!
+  3.times do |i|
+    rect 0, 90 * (2 * i), 90, 90, fill: red
+    rect 0, 90 * (2 * i + 1), 90, 90 , fill: darkblue
   end
 
+  # Helpers
   def reset
     @worms.each do |worm|
       worm.last.text = worm.last.text[0..-2]
@@ -19,13 +28,26 @@ Shoes.app do
     set_current_worm(0)
   end
   
+  def reset_problem(index)
+    i = rand(9)
+    j = rand(10-i)
+
+    worm = @worms[index]
+    worm[-2] = i
+    worm[-3] = j
+    worm.last.text = "#{worm[-2]}+#{worm[-3]}="
+  end
+  
   def set_current_worm(index)
+    index = index % @worms.length
     @worms.each do |worm|
-      worm[0].stroke = black
+      worm.last.hide
     end
-    if @worms[index]
-      @worms[index][0].stroke = red
+
+    if worm = @worms[index]
+      worm.last.toggle
     end
+    
     @current_worm = index
   end
 
@@ -39,12 +61,15 @@ Shoes.app do
       end 
 
       if key == (worm[-2] + worm[-3]).to_s
-        set_current_worm(@current_worm + 1)
-        if @worms[@current_worm].nil?
-          alert "Hurray!"
-          reset
-        end
+        worm.each do |element|
+          element.left -= 20 unless element.is_a?(Integer)
+        end  
+      else
+        Thread.new { `afplay ~/.hacketyhack/beep.mp3` }
       end
+
+      reset_problem(@current_worm)      
+      set_current_worm(@current_worm + 1)
     end
   end  
 end
